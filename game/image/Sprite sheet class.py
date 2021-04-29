94e078437e7b649db49faa66aa6499cc5fde9842
Akoo510 <tomjeannin@hotmail.fr>
2021-04-29 15:20:16 +0200

    update about the game

---
+++ game/image/Sprite sheet class.py
@@ -0,0 +1,38 @@
+import math, random, sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+running = True
+
+while running :
+    for event in pygame.event.get :
+        if event.type == QUIT:
+            running = False
+            
+W, H = 1920,1080
+HW, HH = W/2, H/2
+AREA = W*H
+
+CLOCK = pygame.time.Clock()
+DS = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Sprite Sheets - Akoo")
+FPS = 3
+
+BLACK = (0, 0,0)
+WHITE = (255, 255, 255)
+
+class spritesheet:
+    def __init__(self, filename, cols, rows):
+        self.sheet = pygame.image.load("Soldier.png").convert_alpha()
+        
+        self.cols = cols
+        self.rows = rows
+        self.totalCellCount = cols*rows
+        
+        self.rect = self.sheet.get_rect()
+        w = self.cellWidth = self.rect.width/cols
+        h = self.cellHeight = self.rect.height/rows
+
+        
