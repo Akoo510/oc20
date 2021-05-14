fcbd51b4f442bfbe6bb7d3ef1c43e15e8a575f75
Akoo510 <77661930+Akoo510@users.noreply.github.com>
2021-05-14 14:00:19 +0200

    Updated Code

--- game/image/Sprite sheet class.py
+++ game/image/Sprite sheet class.py
@@ -3,22 +3,15 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-
-running = True
-
-while running :
-    for event in pygame.event.get :
-        if event.type == QUIT:
-            running = False
             
-W, H = 1920,1080
+W, H = 1000,1000
 HW, HH = W/2, H/2
 AREA = W*H
 
 CLOCK = pygame.time.Clock()
 DS = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Sprite Sheets - Akoo")
-FPS = 3
+FPS = 10
 
 BLACK = (0, 0,0)
 WHITE = (255, 255, 255)
@@ -34,5 +27,38 @@ class spritesheet:
         self.rect = self.sheet.get_rect()
         w = self.cellWidth = self.rect.width/cols
         h = self.cellHeight = self.rect.height/rows
+        hw,hh = self.cellCenter = (w/2, h/2)
+        
+        self.cells = list([(Index%cols*w, Index/cols*h, w, h)for Index in range(self.totalCellCount)])
+        self.handle = list([
+            (0, 0), (-hw, 0), (-w, 0),
+            (0, -hh), (-hw, -hh), (-w, -hh),
+            (0, -h), (-hw, -h), (-w, -h),])
+        
+    def draw(self, surface, cellIndex, x, y, handle = 0):
+        surface.blit(self.sheet, (x + self.handle[handle][0], y + self.handle[handle][1]), self.cells[cellIndex])
+        
+s = spritesheet("GreenPlayerRun.png", 8, 1)
+
+CENTER_HANDLE = 4
+
+Index = 0
+
+# Loop
+
+while True :
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+    
+    
+        s.draw(DS, Index % s.totalCellCount, HW, HH, CENTER_HANDLE)
+        Index += 1
+        
+        pygame.draw.circle(DS, WHITE, (HW, HH), 2, 0)
+        
+        pygame.display.update()
+        CLOCK.tick(FPS)
+        DS.fill(BLACK)
 
         
